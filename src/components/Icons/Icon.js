3a1c3387b02f2818b6f8b67156f408e92b86e544
Your Name <you@example.com>
2023-08-25 09:41:14 +0700

    fix file index.js

---
+++ src/components/Icons/Icon.js
@@ -0,0 +1,35 @@
+export const UploadIcon = ({ className }) => (
+    <svg
+        ariahidden="true"
+        focusable="false"
+        dataprefix="fas"
+        dataicon="cloud-arrow-up"
+        className={className}
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 640 512"
+    >
+        <path
+            fill="currentColor"
+            d="M144 480C64.5 480 0 415.5 0 336c0-62.8 40.2-116.2 96.2-135.9c-.1-2.7-.2-5.4-.2-8.1c0-88.4 71.6-160 160-160c59.3 0 111 32.2 138.7 80.2C409.9 102 428.3 96 448 96c53 0 96 43 96 96c0 12.2-2.3 23.8-6.4 34.6C596 238.4 640 290.1 640 352c0 70.7-57.3 128-128 128H144zm79-217c-9.4 9.4-9.4 24.6 0 33.9s24.6 9.4 33.9 0l39-39V392c0 13.3 10.7 24 24 24s24-10.7 24-24V257.9l39 39c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-80-80c-9.4-9.4-24.6-9.4-33.9 0l-80 80z"
+        ></path>
+    </svg>
+);
+
+export const MessageIcon = ({ className }) => (
+    <svg
+        ariahidden="true"
+        focusable="false"
+        dataprefix="fas"
+        dataicon="paper-plane"
+        class="svg-inline--fa fa-paper-plane "
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 512 512"
+    >
+        <path
+            fill="currentColor"
+            d="M498.1 5.6c10.1 7 15.4 19.1 13.5 31.2l-64 416c-1.5 9.7-7.4 18.2-16 23s-18.9 5.4-28 1.6L284 427.7l-68.5 74.1c-8.9 9.7-22.9 12.9-35.2 8.1S160 493.2 160 480V396.4c0-4 1.5-7.8 4.2-10.7L331.8 202.8c5.8-6.3 5.6-16-.4-22s-15.7-6.4-22-.7L106 360.8 17.7 316.6C7.1 311.3 .3 300.7 0 288.9s5.9-22.8 16.1-28.7l448-256c10.7-6.1 23.9-5.5 34 1.4z"
+        ></path>
+    </svg>
+);
